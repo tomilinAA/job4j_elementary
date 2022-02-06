36bfa79a113c99eff869cd509dd35fceb39fdbf6
USER <tomilin-2000@mail.ru>
2022-02-06 14:31:33 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Артём");
+        System.out.println("17.01.2000");
+    }
+}
