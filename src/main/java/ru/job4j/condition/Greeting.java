7715aa8441dcfd1e4722b4e332120f6a2b5d3f9d
USER <tomilin-2000@mail.ru>
2022-02-12 22:32:56 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -2,6 +2,13 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
+    }
+}
+        /*
         String idea = " I like java! ";
         System.out.println(idea);
         String but = idea + "But I am a newbie. ";
@@ -11,3 +18,5 @@ public class Greeting {
 
     }
 }
+
+         */
--- src/main/java/ru/job4j/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/ru/job4j/converter/Converter.java
@@ -24,9 +24,33 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
         float dollar = Converter.rubleToDollar(140);
+        float in1 = 140;
+        float expected1 = 2;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 rubles are 2. Test result : " + passed1);
+
         float yuan = Converter.rubleToYuan(140);
+        float in2 = 140;
+        float expected2 = 2;
+        float out2 = Converter.rubleToYuan(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("140 rubles are 2. Test result : " + passed2);
+
         float tugriki = Converter.rubleToTugriki(140);
+        float in3 = 140;
+        float expected3 = 2;
+        float out3 = Converter.rubleToTugriki(in3);
+        boolean passed3 = expected3 == out3;
+        System.out.println("140 rubles are 2. Test result : " + passed3);
+
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
         System.out.println("140 rubles are " + yuan + " yuan");
