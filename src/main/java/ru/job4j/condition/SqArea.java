04e15553d042e056840aceaa0ec75bedd0ede29d
USER <tomilin-2000@mail.ru>
2022-02-13 20:27:37 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -9,9 +9,38 @@ public class SqArea {
             return rsl3;
         }
 
+        public static double square1(double p, double k) {
+        double rsl = p / (2 * (k + 1));
+        double rsl2 = rsl * k;
+        double rsl3 = rsl2 * rsl;
+        return rsl3;
+
+        }
+
+        public static double square2(double p, double k) {
+        double rsl = p / (2 * (k + 1));
+        double rsl2 = rsl * k;
+        double rsl3 = rsl2 * rsl;
+        return rsl3;
+
+        }
+
+        public static double square3(double p, double k) {
+        double rsl = p / (2 * (k + 1));
+        double rsl2 = rsl * k;
+        double rsl3 = rsl2 * rsl;
+        return rsl3;
+        }
+
         public static void main(String[] args) {
             double result1 = SqArea.square(6, 2);
+            double result2 = SqArea.square1(1, 2);
+            double result3 = SqArea.square2(3, 4);
+            double result4 = SqArea.square3(5, 6);
             System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+            System.out.println(" p = 1, k = 2, s = 0.05, real = " + result2);
+            System.out.println(" p = 3, k = 4, s = 0.36, real = " + result3);
+            System.out.println(" p = 5, k = 6, s = 0.77, real = " + result4);
         }
     }
 
---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP1K2Square0dot05() {
+        double expected = 0.05;
+        int p = 1;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP3K4Square0dot36() {
+        double expected = 0.36;
+        int p = 3;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP5K6quare0dot() {
+        double expected = 0.77;
+        int p = 5;
+        double k = 6;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
