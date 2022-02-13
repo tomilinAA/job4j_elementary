14b9f7b3378d9db8e8add955e0190d532efdcf3d
USER <tomilin-2000@mail.ru>
2022-02-13 20:10:22 +0300

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -10,11 +10,42 @@ public class Point {
         return Math.sqrt(rsl4);
     }
 
+    public static double distance1(int x1, int y1, int x2, int y2) {
+        double rsl = x2 - x1;
+        double rsl1 = y2 - y1;
+        double rsl2 = Math.pow(rsl, 2);
+        double rsl3 = Math.pow(rsl1, 2);
+        double rsl4 = rsl2 + rsl3;
+        return Math.sqrt(rsl4);
+    }
+
+    public static double distance2(int x1, int y1, int x2, int y2) {
+        double rsl = x2 - x1;
+        double rsl1 = y2 - y1;
+        double rsl2 = Math.pow(rsl, 2);
+        double rsl3 = Math.pow(rsl1, 2);
+        double rsl4 = rsl2 + rsl3;
+        return Math.sqrt(rsl4);
+    }
+
+    public static double distance3(int x1, int y1, int x2, int y2) {
+        double rsl = x2 - x1;
+        double rsl1 = y2 - y1;
+        double rsl2 = Math.pow(rsl, 2);
+        double rsl3 = Math.pow(rsl1, 2);
+        double rsl4 = rsl2 + rsl3;
+        return Math.sqrt(rsl4);
+    }
+
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         double result1 = Point.distance(0, 4, 2, 3);
+        double result2 = Point.distance(1, 2, 2, 3);
+        double result3 = Point.distance(2, 2, 4, 4);
         System.out.println("result (0, 0) to (2, 0) " + result);
         System.out.println("result (0, 3) to (4, 3) "  + result1);
+        System.out.println("result (1, 2) to (2, 3) "  + result2);
+        System.out.println("result (2, 2) to (4, 4) "  + result3);
     }
 }
 
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when04to23then() {
+        double expected = 2.23;
+        int x1 = 0;
+        int y1 = 4;
+        int x2 = 2;
+        int y2 = 3;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when12to23then() {
+        double expected = 1.41;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 2;
+        int y2 = 3;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when22to44then() {
+        double expected = 2.82;
+        int x1 = 2;
+        int y1 = 2;
+        int x2 = 4;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
