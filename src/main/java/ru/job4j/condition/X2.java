a232c70a9fcb14f6ca358384ea8fa91b3b17153f
USER <tomilin-2000@mail.ru>
2022-02-14 16:53:40 +0300

    1.2. Входные данные в тесте [#297750 #264192]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -5,49 +5,17 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
-    public static int calc1(int a1, int b1, int c1, int x1) {
-        return a1 * x1 * x1 + b1 * x1 + c1;
-
-    }
-
-    public static int calc2(int a2, int b2, int c2, int x2) {
-        return a2 * x2 * x2 + b2 * x2 + c2;
-    }
-
-    public static int calc3(int a3, int b3, int c3, int x3) {
-        return a3 * x3 * x3 + b3 * x3 + c3;
-    }
-
-    public static int calc4(int a4, int b4, int c4, int x4) {
-        return a4 * x4 * x4 + b4 * x4 + c4;
-    }
-
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
         int c = 0;
         int x = 2;
-        int a1 = 1;
-        int b1 = 1;
-        int c1 = 1;
-        int x1 = 1;
-        int a2 = 0;
-        int b2 = 1;
-        int c2 = 1;
-        int x2 = 1;
-        int a3 = 1;
-        int b3 = 1;
-        int c3 = 0;
-        int x3 = 1;
-        int a4 = 1;
-        int b4 = 1;
-        int c4 = 1;
-        int x4 = 0;
+
         int rsl = X2.calc(a, b, c, x);
-        int rsl1 = X2.calc1(a1, b1, c1, x1);
-        int rsl2 = X2.calc2(a2, b2, c2, x2);
-        int rsl3 = X2.calc3(a3, b3, c3, x3);
-        int rsl4 = X2.calc4(a4, b4, c4, x4);
+        int rsl1 = X2.calc(1, 1, 1, 1);
+        int rsl2 = X2.calc(0, 1, 1, 1);
+        int rsl3 = X2.calc(1, 1, 0, 1);
+        int rsl4 = X2.calc(1, 1, 1, 0);
         System.out.println(rsl);
         System.out.println(rsl1);
         System.out.println(rsl2);
