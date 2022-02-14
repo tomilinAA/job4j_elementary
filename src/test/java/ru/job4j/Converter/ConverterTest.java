f67ebafb85fb8a616537be928061b08591fc7ea6
USER <tomilin-2000@mail.ru>
2022-02-14 10:15:32 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/Converter/ConverterTest.java
+++ src/test/java/ru/job4j/Converter/ConverterTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.ru.job4j.converter;
+package ru.job4j.Converter;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,7 +9,7 @@ public class ConverterTest {
     public void whenConvert140RblThen2Euro() {
             float in = 140;
             float expected = 2;
-            float out = ru.job4j.converter.Converter.rubleToEuro(in);
+            float out = Converter.rubleToEuro(in);
             float eps = 0.0001f;
             Assert.assertEquals(expected, out, eps);
     }
@@ -18,7 +18,7 @@ public class ConverterTest {
     public void whenConvert140RblThen2Dollar() {
                 float in1 = 140;
                 float expected1 = 2;
-                float out1 = ru.job4j.converter.Converter.rubleToEuro(in1);
+                float out1 = Converter.rubleToEuro(in1);
                 float eps1 = 0.0001f;
                 Assert.assertEquals(expected1, out1, eps1);
             }
@@ -27,7 +27,7 @@ public class ConverterTest {
     public void whenConvert140RblThen2Yuan() {
         float in2 = 140;
         float expected2 = 2;
-        float out2 = ru.job4j.converter.Converter.rubleToEuro(in2);
+        float out2 = Converter.rubleToEuro(in2);
         float eps2 = 0.0001f;
         Assert.assertEquals(expected2, out2, eps2);
 
@@ -37,7 +37,7 @@ public class ConverterTest {
     public void whenConvert140RblThen2Tugriki() {
         float in3 = 140;
         float expected3 = 2;
-        float out3 = ru.job4j.converter.Converter.rubleToEuro(in3);
+        float out3 = Converter.rubleToEuro(in3);
         float eps3 = 0.0001f;
         Assert.assertEquals(expected3, out3, eps3);
     }
