a134f5534a12902ee4d063567a82155964965ec5
USER <tomilin-2000@mail.ru>
2022-02-14 21:45:26 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,7 +4,7 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            rsl = "Привет,умник.";
+            rsl = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
