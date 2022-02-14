ec62d4895e544ec5bac8cbebf9d5abe0be134cfb
USER <tomilin-2000@mail.ru>
2022-02-14 17:53:10 +0300

    4.1. Аргументы [#295521 #263675]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -2,8 +2,8 @@ package ru.job4j.calculator;
 
 public class ArgMethod {
 
-        public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
+    public static void hello(String name, int age) {
+            System.out.println("Hello, " + name + ", age = " + age);
     }
 
     public static void main(String[] args) {
