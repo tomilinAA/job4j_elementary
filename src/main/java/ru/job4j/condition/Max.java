a59470907316f1ae777fc28f2730b16ff498496c
USER <tomilin-2000@mail.ru>
2022-02-15 13:00:58 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+
+    }
+
+    public static void main(String[] args) {
+        int check = Max.max(2, 1);
+        System.out.println(check);
+    }
+}
