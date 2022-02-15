4cf5823a192a8caf54b3e860d9fd9c1e45aec191
USER <tomilin-2000@mail.ru>
2022-02-15 13:29:03 +0300

    4.3.2. Оператор || [#333784]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -4,7 +4,7 @@ public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
         if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
-        } else {
+        } else  {
             System.out.println("I can't.");
         }
     }
