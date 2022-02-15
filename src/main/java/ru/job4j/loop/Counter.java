ab837c1fb39667abde120abc016f5b739e97a8f7
USER <tomilin-2000@mail.ru>
2022-02-15 21:51:58 +0300

    5.0.1. Сумма чисел. [#226416]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -13,5 +13,7 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+        System.out.println(sum(2, 15));
+        System.out.println(sum(0, 5));
     }
 }
