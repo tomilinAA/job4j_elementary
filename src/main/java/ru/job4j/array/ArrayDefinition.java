4cb21bc9d0f218c75479239a9170d4d0e819052e
USER <tomilin-2000@mail.ru>
2022-02-19 14:42:22 +0300

    6.0.1. Объявление массива. [#3613 #266609]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,19 +3,19 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Значение массива ages = " + ages.length);
         String[] surnames = new String[100500];
-        System.out.println("Значение массива surnames = " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Значение массива ages = " + ages.length);
+        System.out.println("Значение массива surnames = " + surnames.length);
         System.out.println("Значение массива prices = " + prices.length);
         String[] names = new String[4];
         names[0] = "Алёша";
-        System.out.println(names[0]);
         names[1] = "Коля";
-        System.out.println(names[1]);
         names[2] = "Баир";
-        System.out.println(names[2]);
         names[3] = "Нурмаджонсенсенбай";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
         System.out.println(names[3]);
     }
 }
