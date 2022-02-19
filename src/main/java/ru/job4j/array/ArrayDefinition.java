69e764a3001b751f71fe1f0cfba0e484423a811e
USER <tomilin-2000@mail.ru>
2022-02-19 11:15:44 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,15 @@ public class ArrayDefinition {
         System.out.println("Значение массива surnames = " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Значение массива prices = " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Алёша";
+        System.out.println(names[0]);
+        names[1] = "Коля";
+        System.out.println(names[1]);
+        names[2] = "Баир";
+        System.out.println(names[2]);
+        names[3] = "Нурмаджонсенсенбай";
+        System.out.println(names[3]);
     }
 }
 
