cee1385c0df8aafe8670949e1e7b3b102fd19d58
USER <tomilin-2000@mail.ru>
2022-02-19 11:06:58 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,15 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        System.out.println("Значение массива ages = " + ages.length);
+        String[] surnames = new String[100500];
+        System.out.println("Значение массива surnames = " + surnames.length);
+        float[] prices = new float[40];
+        System.out.println("Значение массива prices = " + prices.length);
+    }
 }
+
+
+
