b26521433d5d493bf43e08a124140a913966437a
USER <tomilin-2000@mail.ru>
2022-02-19 12:56:00 +0300

    6.0.5. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -7,7 +7,7 @@ public class Square {
     }
 
     public static void main(String[] args) {
-        int[] array = calculate(4);
+        int[] array = calculate(5);
         for (int index = 0; index < array.length; index++) {
             array[index] = index * index;
             System.out.println(array[index]);
